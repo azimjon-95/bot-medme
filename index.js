3ef616b6f4b7ade438798211d6c0de42c7fb1e0a
azimjon-95 <>
2024-08-20 15:09:16 +0500

    start

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const express = require("express");
 const { connect } = require("mongoose");
 require("dotenv").config();
 require("colors");
-
-const app = express();
-app.use(express.json());
-
 
 async function connectToDb() {
     // set("strictQuery", false);
@@ -15,12 +10,6 @@ async function connectToDb() {
 }
 
 connectToDb();
-
-app.get("/", async (req, res) => res.json("App is running"));
-
 require('./controllers/botCtrl')
 
-const PORT = process.env.PORT || 5050;
-app.listen(PORT, () =>
-    console.log(`Server listening => http://localhost:${PORT}`.bgCyan)
-);
+
