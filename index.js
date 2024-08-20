7e8d7ca4e4df8b2551717a7c47c380a8ccd99a54
azimjon-95 <>
2024-08-20 15:25:31 +0500

    delete colors

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const { connect } = require("mongoose");
 require("dotenv").config();
-require("colors");
 
 async function connectToDb() {
     // set("strictQuery", false);
     await connect(process.env.MONGO_URL)
-        .then(() => console.log("MongoDB is connected".bgGreen.white))
-        .catch((err) => console.log("MongoDB is not connected".bgRed.white, err));
+        .then(() => console.log("MongoDB is connected"))
+        .catch((err) => console.log("MongoDB is not connected", err));
 }
 
 connectToDb();
